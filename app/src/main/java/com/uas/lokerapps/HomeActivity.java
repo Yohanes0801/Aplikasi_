a9746f87e8d563958be61ca7f71da7b612693934
absor <dwiyanaabsor@gmail.com>
2023-07-05 14:59:06 +0700

    Update HomeActivity ke JSON list

--- app/src/main/java/com/uas/lokerapps/HomeActivity.java
+++ app/src/main/java/com/uas/lokerapps/HomeActivity.java
@@ -1,10 +1,18 @@
 package com.uas.lokerapps;
 
+import android.content.res.AssetManager;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,24 +32,65 @@ public class HomeActivity extends AppCompatActivity {
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
         jobRecyclerView.setLayoutManager(layoutManager);
 
-        // Buat daftar pekerjaan dan tambahkan item
-        List<Job> jobList = createJobList();
+        // Buat daftar pekerjaan dan tambahkan item dari JSON
+        List<Job> jobList = createJobListFromJson();
 
         // Inisialisasi adapter dan atur ke RecyclerView
         jobAdapter = new JobAdapter(jobList);
         jobRecyclerView.setAdapter(jobAdapter);
     }
 
-    private List<Job> createJobList() {
+    private List<Job> createJobListFromJson() {
         List<Job> jobList = new ArrayList<>();
 
-        // Tambahkan item pekerjaan ke daftar
-        jobList.add(new Job(R.drawable.logo_mayora, "Software Engineer", "Google", "Lorem ipsum dolor sit amet."));
-        jobList.add(new Job(R.drawable.logo_mayora, "Product Manager", "Facebook", "Lorem ipsum dolor sit amet."));
-        jobList.add(new Job(R.drawable.logo_mayora, "UI/UX Designer", "Apple", "Lorem ipsum dolor sit amet."));
+        try {
+            // Membaca file JSON dari folder assets
+            String json = loadJSONFromAsset("jobs.json");
 
-        // Tambahkan lebih banyak item pekerjaan sesuai kebutuhan
+            // Parsing data JSON menjadi objek
+            JSONArray jsonArray = new JSONArray(json);
+
+            // Iterasi melalui setiap objek dalam array JSON
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                // Mendapatkan nilai dari setiap kunci JSON
+                int logo = getResourceId(jsonObject.getString("logo"), "drawable");
+                String position = jsonObject.getString("position");
+                String company = jsonObject.getString("company");
+                String description = jsonObject.getString("description");
+
+                // Membuat objek Job dan menambahkannya ke daftar
+                Job job = new Job(logo, position, company, description);
+                jobList.add(job);
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
         return jobList;
     }
+
+    private String loadJSONFromAsset(String fileName) {
+        String json;
+        try {
+            // Membaca file JSON dari folder assets
+            InputStream inputStream = getAssets().open(fileName);
+            int size = inputStream.available();
+            byte[] buffer = new byte[size];
+            inputStream.read(buffer);
+            inputStream.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return json;
+    }
+
+    private int getResourceId(String resourceName, String resourceType) {
+        return getResources().getIdentifier(resourceName, resourceType, getPackageName());
+    }
 }
+
